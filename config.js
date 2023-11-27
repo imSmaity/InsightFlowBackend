fb8588280ea9c65937d755d8cd8e2dd0b336a40e
imSmaity <sumanmaity305@gmail.com>
2023-11-28 02:09:56 +0530

    build

--- config.js
+++ config.js
@@ -6,20 +6,6 @@ module.exports = {
   DB_USER: process.env.DB_USER || '',
   DB_PASSWORD: process.env.DB_PASSWORD || '',
   DB_NAME: process.env.DB_NAME || '',
-  PRIVATE_KEY: (() => {
-    try {
-      return fs.readFileSync('private.key', 'utf8');
-    } catch (err) {
-      console.log('[error] private.key file not found.', err);
-      return null;
-    }
-  })(),
-  PUBLIC_KEY: (() => {
-    try {
-      return fs.readFileSync('public.key', 'utf8');
-    } catch (err) {
-      console.log('[error] public.key file not found.', err);
-      return null;
-    }
-  })(),
+  PRIVATE_KEY: process.env.PRIVATE_KEY || '',
+  PUBLIC_KEY: process.env.PUBLIC_KEY || '',
 };
